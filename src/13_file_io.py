8f33eb1fb987561bbcea84c523fd0fd0d431b7f7
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-11-18 18:28:11 -0800

    Solution for #13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('src/foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+myFile = open('src/bar.txt', "w")
+
+lines = ['First line here.\n',
+         'Then second line.\n',
+         'Is it enough?\n',
+         'I\'m tired...']
+for i in lines:
+    myFile.write(i)
+
+myFile.close()
+
+with open('src/bar.txt') as b:
+    read_data = b.read()
+    print(read_data)
+    b.closed
